d7082383a1a95c7d4921a1406bc2fda558858bb5
Siggi88 <7286178+Siggi88@users.noreply.github.com>
2020-10-04 17:38:44 +0800

    Unbreak the build

--- src/main/java/io/siggi/testjenkins/Main.java
+++ src/main/java/io/siggi/testjenkins/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("This is a test!");
 		System.out.println(":o");
-		s
+		System.out.println("It works again!");
 	}
 }
