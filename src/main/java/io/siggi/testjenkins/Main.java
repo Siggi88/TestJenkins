ac0144ba0d78ecca0e5f0c75adbef685e9d7d308
Siggi88 <7286178+Siggi88@users.noreply.github.com>
2020-10-04 17:39:56 +0800

    I like peanut butter cups

--- src/main/java/io/siggi/testjenkins/Main.java
+++ src/main/java/io/siggi/testjenkins/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("This is a test!");
 		System.out.println(":o");
-		System.out.println("It works again!");
+		System.out.println("I like peanut butter cups");
 	}
 }
