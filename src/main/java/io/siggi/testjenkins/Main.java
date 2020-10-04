84fa432c2fae12b881ec6639cd799975f9f573cc
Siggi88 <7286178+Siggi88@users.noreply.github.com>
2020-10-04 17:35:20 +0800

    Change D: to :o

--- src/main/java/io/siggi/testjenkins/Main.java
+++ src/main/java/io/siggi/testjenkins/Main.java
@@ -3,6 +3,6 @@ package io.siggi.testjenkins;
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("This is a test!");
-		System.out.println("D:");
+		System.out.println(":o");
 	}
 }
