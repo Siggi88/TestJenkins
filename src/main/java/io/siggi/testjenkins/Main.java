f106aae9f58e9b36d0f33668d9e90b6577c42150
Siggi88 <7286178+Siggi88@users.noreply.github.com>
2020-10-04 17:32:20 +0800

    Change xD to D:

--- src/main/java/io/siggi/testjenkins/Main.java
+++ src/main/java/io/siggi/testjenkins/Main.java
@@ -3,6 +3,6 @@ package io.siggi.testjenkins;
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("This is a test!");
-		System.out.println("xD");
+		System.out.println("D:");
 	}
 }
